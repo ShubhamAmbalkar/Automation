289cb841a39dc3cea1aed7bd97271a2e6091fa66
ShubhamAmbalkar <102300810+ShubhamAmbalkar@users.noreply.github.com>
2022-10-06 20:18:49 +0530

    Update test_add.py

--- test_add.py
+++ test_add.py
@@ -1,8 +1,6 @@
 # Swag Demo TEST
 # testing
 import pytest
-import allure
-from allure_commons.types import AttachmentType
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -16,7 +14,6 @@ def test_setup():
     driver.maximize_window()
 
 
-@allure.severity(allure.severity_level.CRITICAL)
 def test_validLogin(test_setup):
     driver.get('https://www.saucedemo.com')
     driver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys('standard_user')
@@ -29,15 +26,12 @@ def test_validLogin(test_setup):
     driver.close()
 
 
-@allure.severity(allure.severity_level.CRITICAL)
 def test_invalidLogin(test_setup):
     driver.get('https://www.saucedemo.com')
     driver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys('standard_user')
     driver.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_auce')
     driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
     if driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/div[2]').is_displayed():
-        allure.attach(driver.get_screenshot_as_png(), name='test_login_failed',
-                      attachment_type=AttachmentType.PNG)
         driver.save_screenshot('C://Users//Austin//PycharmProjects//Python//InvalidLogin.png')
         assert True
     else:
@@ -45,7 +39,6 @@ def test_invalidLogin(test_setup):
     driver.close()
 
 
-@allure.severity(allure.severity_level.CRITICAL)
 def test_addtocart(test_setup):
     driver.get('https://www.saucedemo.com')
     driver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys('standard_user')
@@ -56,8 +49,6 @@ def test_addtocart(test_setup):
     driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a').click()
 
     if driver.find_element(By.XPATH, '//*[@id="item_4_title_link"]/div').is_displayed():
-        allure.attach(driver.get_screenshot_as_png(), name='Add_to_Cart.png',
-                      attachment_type=AttachmentType.PNG)
         driver.save_screenshot('C://Users//Austin//PycharmProjects//Python//Add_To_Cart.png')
         assert True
     else:
